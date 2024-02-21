48b5b72be1384207d3ebe18995c476924ab0c59c
Matt Zuk <matt@ip0af54953.int.uvm.edu>
2024-02-20 19:58:46 -0500

    Starting work on simulation.

--- simulate.py
+++ simulate.py
@@ -1,5 +1,19 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+
+p.loadSDF("box.sdf")
+
+# Iterate 1000 times and step the simulation in each iteration
+for i in range(1000):
+    # Print the loop variable
+    print("Loop variable:", i)
+    
+    # Step the simulation
+    p.stepSimulation()
+
+    # Introduce a delay of 1/60th of a second
+    time.sleep(1/60)
 
 p.disconnect()
